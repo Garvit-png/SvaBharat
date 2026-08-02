e42164e3e60cb3674b1b5ebf6b438a50549ea76c
Japinder Kaur <japinderofficial@gmail.com>
2026-08-02 12:34:49 +0530

    Fix image name from 7.png to img7.png on Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -26,7 +26,7 @@ export function Contact() {
               </p>
             </div>
                         <div className="flex-1 w-full flex items-end justify-start mt-8 opacity-90 mix-blend-multiply pb-10">
-              <img src="/7.png" alt="Get in touch illustration" className="w-[85%] max-w-md object-contain" />
+              <img src="/img7.png" alt="Get in touch illustration" className="w-[85%] max-w-md object-contain" />
             </div>
             {/* Direct Contact Info (Temporarily Hidden) */}
             {false && (
